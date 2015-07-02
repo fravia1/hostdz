8aecf63ea984243a6b5aa8bcb6766a11c447e2a7
fjdhgjaf <tiby0108@gmail.com>
2015-07-02 07:37:56 +0200

    -
    -

---
+++ otherdata.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php -q
+<?php
+ignore_user_abort(true);
+set_time_limit(0);
+error_reporting(0);
+ini_set('display_errors', '0');
+
+if (file_exists("/etc/hostdz/users/".$argv[1].".info")){
+	$filearray = explode("\n", file_get_contents("/etc/hostdz/users/".$argv[1].".info", true));
+	foreach ($filearray as $value) {
+		if (preg_match('/SABNZBD HTTPS port:/i', $value)) {
+			echo substr($value, 5);;
+		}
+	}
+}else
+	echo "No user with id.";
+?>
